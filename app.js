7236116c8ddac0bc5cf92d67d505da1010240dba
Low Lowlow <lowyinghong@Low-MacBook.local>
2026-08-20 02:25:27 +0800

    Enable automatic app update cleanup

--- app.js
+++ app.js
@@ -185,4 +185,14 @@
   renderRecent(getRecent());
   loadRecent();
   loadBalance();
+
+  // Keep installed versions aligned with the latest GitHub Pages files.
+  if ("serviceWorker" in navigator) {
+    window.addEventListener("load", () => {
+      navigator.serviceWorker.getRegistrations().then((registrations) => {
+        registrations.forEach((registration) => registration.unregister());
+      }).catch(() => {});
+      if ("caches" in window) caches.keys().then((keys) => keys.forEach((key) => caches.delete(key))).catch(() => {});
+    });
+  }
 })();
